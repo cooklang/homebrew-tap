ea95089e0edd262d68fea4e54c98a4455ececbd3
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2021-11-03 21:22:02 +0000

    Add bottle block
    Closes #5.
    
    Signed-off-by: Alexey Dubovskoy <dubovskoy.a@gmail.com>

--- Formula/cook.rb
+++ Formula/cook.rb
@@ -6,6 +6,12 @@ class Cook < Formula
 
   head "https://github.com/cooklang/CookCLI.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/cooklang/homebrew-tap/releases/download/cook-0.0.11"
+    sha256 cellar: :any_skip_relocation, big_sur:     "8b4780f8bcedf64e31e3eff64788d4ec8637e22dc9894f2ebd4fc5732ecc1471"
+    sha256 cellar: :any_skip_relocation, catalina:    "8b4780f8bcedf64e31e3eff64788d4ec8637e22dc9894f2ebd4fc5732ecc1471"
+  end
+
   depends_on xcode: ["10.0", :build]
 
   def install
