820eb16b07a22bcc78a5c56ee202022d11dde9a5
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2021-11-03 20:05:17 +0000

    Add workflow to publish bottles

--- Formula/cook.rb
+++ Formula/cook.rb
@@ -1,7 +1,7 @@
 class Cook < Formula
   desc "CLI tool for CookLang Recipe Markup Language"
   homepage "https://cooklang.org"
-  url "https://github.com/cooklang/CookCLI.git", tag: "v0.0.9"
+  url "https://github.com/cooklang/CookCLI.git", tag: "v0.0.11"
   license "MIT"
 
   head "https://github.com/cooklang/CookCLI.git", branch: "main"
@@ -11,25 +11,9 @@ class Cook < Formula
   patch :DATA
 
   def install
-    system "make", "build_macos"
-    bin.install ".build/#{ENV["HOMEBREW_PROCESSOR"]}-apple-macosx/release/cook"
+    system "swift", "build", "--configuration", "release", "--disable-sandbox", "--arch", "arm64", "--arch", "x86_64"
+    bin.install ".build/apple/Products/Release/cook"
   end
 
   test { system "#{bin}/cook", "version" }
 end
-
-__END__
-diff --git a/Makefile b/Makefile
-index 46f905d..ceabce1 100644
---- a/Makefile
-+++ b/Makefile
-@@ -35,7 +35,7 @@ smoketest_linux:
- 
- 
- build_macos:
--	swift build --configuration release  --static-swift-stdlib
-+	swift build --configuration release --disable-sandbox
- 
- archive_macos:
- 	cd .build/x86_64-apple-macosx/release/ && zip "CookCLI_$(VERSION)_darwin_amd64.zip" cook
-
