951dd6975a04560e263fde6157070365e79fec98
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2021-11-14 19:14:17 +0000

    New release v0.0.13
    Closes #7.
    
    Signed-off-by: Alexey Dubovskoy <dubovskoy.a@gmail.com>

--- Formula/cook.rb
+++ Formula/cook.rb
@@ -1,7 +1,7 @@
 class Cook < Formula
   desc "CLI tool for CookLang Recipe Markup Language"
   homepage "https://cooklang.org"
-  url "https://github.com/cooklang/CookCLI.git", tag: "v0.0.12"
+  url "https://github.com/cooklang/CookCLI.git", tag: "v0.0.13"
   license "MIT"
 
   head "https://github.com/cooklang/CookCLI.git", branch: "main"
