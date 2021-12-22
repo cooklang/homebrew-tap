5e722889222c5285d6e3e096b049e460f17b0022
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2021-12-22 20:30:03 +0000

    Update to v0.1.2
    Closes #10.
    
    Signed-off-by: Alexey Dubovskoy <dubovskoy.a@gmail.com>

--- Formula/cook.rb
+++ Formula/cook.rb
@@ -1,13 +1,13 @@
 class Cook < Formula
   desc "CLI tool for CookLang Recipe Markup Language"
   homepage "https://cooklang.org"
-  url "https://github.com/cooklang/CookCLI.git", tag: "v0.1.1"
+  url "https://github.com/cooklang/CookCLI.git", tag: "v0.1.2"
   license "MIT"
 
   head "https://github.com/cooklang/CookCLI.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/cooklang/homebrew-tap/releases/download/cook-0.1.1"
+    root_url "https://github.com/cooklang/homebrew-tap/releases/download/cook-0.1.2"
     sha256 cellar: :any_skip_relocation, catalina: "affab81382acfb1b2dcd3be4982f96ab7f7be9a8a3f68feb1454881f47d1435d"
   end
 
