4f91bbb0d4e08baf1a89909f987d99d0f3895e42
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2022-06-07 20:02:32 +0000

    cook: update 0.1.5 bottle.

--- Formula/cook.rb
+++ Formula/cook.rb
@@ -8,7 +8,7 @@ class Cook < Formula
 
   bottle do
     root_url "https://github.com/cooklang/homebrew-tap/releases/download/cook-0.1.5"
-    sha256 cellar: :any_skip_relocation, big_sur: "5720ea344c3b97bfe75003edf8a6bb7420e22eff0380e45ea4657962b0546aeb"
+    sha256 cellar: :any_skip_relocation, big_sur: "a9e4e706c1fa8771ed643c1f2c41ad0857616c36b1dfae1035412a32b71e96b4"
   end
 
   depends_on xcode: ["10.0", :build]
