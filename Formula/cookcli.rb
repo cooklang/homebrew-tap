a51d4b143625d23b08c5ae96d0af17d2cc3565ca
Caleb Hearth <caleb@calebhearth.com>
2024-11-21 09:23:26 -0700

    cookcli: Update to v0.8.0
    I've actually never built a formula before but I found that this was
    stuck on v0.1.6, so I wanted to take a whack at it. I used the
    [cookbook] (hehe) to generate a new formula based on the tarball, so it
    also renamed this to match the repo name. That's easy enough to undo but
    I've included a [rename] entry for that as well.
    
    [cookbook]: https://docs.brew.sh/Formula-Cookbook
    [rename]: https://docs.brew.sh/Rename-A-Formula
    
    This version will build the UI and CLI directly, and obviously uses NPM
    and Cargo rather than Swift.
    
    cookcli: rename from cook

---
+++ Formula/cookcli.rb
@@ -0,0 +1,22 @@
+class Cookcli < Formula
+  desc "A suite of tools to create shopping lists and maintain recipes"
+  homepage "https://cooklang.org"
+  url "https://github.com/cooklang/cookcli/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "050fcbd7f8f938bd6ffc898a403795101807cfa6d76c787e991c8d90031405c6"
+  license "MIT"
+
+  depends_on "npm" => :build
+  depends_on "rust" => :build
+
+  def install
+    cd "ui" do
+      system "npm", "install" # rubocop:disable Style/DisableCopsWithinSourceCodeDirective FormulaAudit/StdNpmArgs
+      system "npm", "run", "build"
+    end
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system bin/"cook", "version"
+  end
+end
