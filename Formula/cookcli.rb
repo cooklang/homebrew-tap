6a115fa52172b4129deef9ef5f222720725e0e31
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2024-11-21 17:39:39 +0000

    Error: FormulaAudit/Desc: Description shouldn't start with an article.

--- Formula/cookcli.rb
+++ Formula/cookcli.rb
@@ -1,5 +1,5 @@
 class Cookcli < Formula
-  desc "A suite of tools to create shopping lists and maintain recipes"
+  desc "CLI tool to create shopping lists and maintain recipes"
   homepage "https://cooklang.org"
   url "https://github.com/cooklang/cookcli/archive/refs/tags/v0.8.0.tar.gz"
   sha256 "050fcbd7f8f938bd6ffc898a403795101807cfa6d76c787e991c8d90031405c6"
